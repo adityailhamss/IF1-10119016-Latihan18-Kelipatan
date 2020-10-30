12de40bac5d776d6a89b3e0656a3fac72b5b5d8a
aditi <aditi@DESKTOP-343N0FS>
2020-10-31 00:24:11 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan18/kelipatan/IF110119016Latihan18Kelipatan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan18.kelipatan;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan cara 
+ * melakukan kelipatan
+ */
+public class IF110119016Latihan18Kelipatan {
+    
+    public static void main(String[] args) {
+        int bil;
+      
+         for(bil =1; bil <=10;bil++){
+            System.out.println(bil*3.5 + " ");
+        }
+    }
+    
+}
